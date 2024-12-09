96293b3fa3fcf738af5af10dbb29dbcf83db2526
Rohan <rkrohanrk065@gmail.com>
2024-12-09 17:30:00 -0500

    app logo in app bar

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -17,7 +17,21 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       leading: appBarLeading ?? Container(),
-      title: Text(appBarText),
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipOval(
+            child: Image.asset(
+              'assets/earthbnb.png', // Path to your app icon
+              height: 40.0, // Adjust the size
+              width: 40.0,
+              fit: BoxFit.cover, // Ensures the image fits properly in the circle
+            ),
+          ),
+          const SizedBox(width: 8), // Spacing between icon and text
+          Text(appBarText),
+        ],
+      ),
       centerTitle: true,
       backgroundColor: AppColors.accentTeal,
       foregroundColor: AppColors.backgroundWhite,
